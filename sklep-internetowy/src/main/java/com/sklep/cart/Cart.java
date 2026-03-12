e5dfa221a66ed4f1b8ca24a95f14ea2dcba98e47
akatsukiAti <141030100+akatsukiAti@users.noreply.github.com>
2026-03-12 13:27:36 +0100

    feat: cart and iterator

--- sklep-internetowy/src/main/java/com/sklep/cart/Cart.java
+++ sklep-internetowy/src/main/java/com/sklep/cart/Cart.java
@@ -3,8 +3,32 @@ package com.sklep.cart;
 import com.sklep.model.Product;
 
 import java.util.LinkedList;
+import java.util.Optional;
 
 public class Cart {
     LinkedList<Product> productLinkedList = new LinkedList<>();
+    CartIterator iterator = new CartIterator(productLinkedList);
 
+
+    boolean addProduct(Product product){
+    Optional<Product> productToAdd = Optional.ofNullable(product);
+        if(productToAdd.isPresent()){
+            productLinkedList.add(productToAdd.get());
+            return true;
+        }
+
+        return false;
+    }
+
+    boolean removeProduct(Product product){
+        Optional<Product> productToRemove = Optional.ofNullable(product);
+        if(productToRemove.isPresent()){
+            productLinkedList.remove(productToRemove.get());
+            return true;
+        }
+        return false;
+    }
+    Double getTotal(){
+        return productLinkedList.stream().mapToDouble(Product::getPrice).sum();
+    }
 }
