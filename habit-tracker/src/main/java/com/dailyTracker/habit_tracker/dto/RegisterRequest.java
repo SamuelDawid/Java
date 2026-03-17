34b36812eb254594379128f4290326de773a445b
akatsukiAti <141030100+akatsukiAti@users.noreply.github.com>
2026-03-17 10:48:32 +0100

    feat: JWT Auth, register, login, password hashing

---
+++ habit-tracker/src/main/java/com/dailyTracker/habit_tracker/dto/RegisterRequest.java
@@ -0,0 +1,8 @@
+package com.dailyTracker.habit_tracker.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    String userName,email,password;
+}
---
+++ habit-tracker/src/main/java/com/dailyTracker/habit_tracker/security/JwUtil.java
@@ -0,0 +1,52 @@
+package com.dailyTracker.habit_tracker.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+    public boolean isTokenValid(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+}
