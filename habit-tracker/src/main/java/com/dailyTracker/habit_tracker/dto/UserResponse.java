df1a6e2dfc06004879532acc3226f5d34222f628
akatsukiAti <141030100+akatsukiAti@users.noreply.github.com>
2026-03-18 10:07:24 +0100

    feat: frontend connected to API, habits load from DB, checkboxes save to DB

---
+++ habit-tracker/src/main/java/com/dailyTracker/habit_tracker/dto/UserResponse.java
@@ -0,0 +1,18 @@
+package com.dailyTracker.habit_tracker.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String userName;
+    private String email;
+    private LocalDateTime createdAt;
+}
